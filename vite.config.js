254d4fe5c7dbacc7afda593fa82bc9b64fc723fc
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-03-22 21:00:03 +0100

    Add project dependences

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
-})
+  plugins: [
+    vue(),
+    viteCompression(), // by default using gzip
+    viteCompression({ algorithm: "brotliCompress", ext: ".br" }),
+  ],
+});
+
