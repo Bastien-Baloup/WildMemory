c761f8354eb82d5ee648b7d075f993c298e534da
Bastien-Baloup <35816948+Bastien-Baloup@users.noreply.github.com>
2022-03-28 14:59:56 +0200

    Add emoji font
    Add an emoji font to ensure flag emojies are displayed on windows 10+

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,22 @@
 /* google fonts */
 @import url("https://fonts.googleapis.com/css2?family=Pacifico&family=Ultra&display=swap");
 .pacifico {
-  font-family: "Pacifico", cursive;
+  font-family: "emoji", "Pacifico", cursive;
 }
 .ultra {
-  font-family: "Ultra", serif;
+  font-family: "emoji", "Ultra", serif;
+}
+
+/* emoji font */
+
+@font-face {
+  font-family: "emoji";
+  src: url("/fonts/NotoColorEmoji.ttf") format("truetype");
+  unicode-range: U+0080-02AF, U+0300-03FF, U+0600-06FF, U+0C00-0C7F, U+1DC0-1DFF,
+    U+1E00-1EFF, U+2000-209F, U+20D0-214F, U+2190-23FF, U+2460-25FF, U+2600-27EF,
+    U+2900-29FF, U+2B00-2BFF, U+2C60-2C7F, U+2E00-2E7F, U+3000-303F, U+A490-A4CF,
+    U+E000-F8FF, U+FE00-FE0F, U+FE30-FE4F, U+1F000-1F02F, U+1F0A0-1F0FF,
+    U+1F100-1F64F, U+1F680-1F6FF, U+1F910-1F96B, U+1F980-1F9E0;
 }
 
 /* tailwind imports */
@@ -20,7 +32,7 @@
 @tailwind utilities;
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "emoji", Avenir, Helvetica, Arial, sans-serif;
   @apply scroll-smooth antialiased h-screen;
 }
 
